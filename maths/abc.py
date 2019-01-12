c7f1a700cf665b29dd5a4c45d64ac8a0ba4bbc67
BraverChiang <huanfa2020@gmail.com>
2019-01-12 23:40:03 +0800

    \math

---
+++ maths/abc.py
@@ -0,0 +1,14 @@
+class myclass(object):
+    """docstring fs myclass."""
+    def __init__(self, arg):
+        sups myclass, self).__init__()
+        self.arg = arg
+
+
+
+
+    def fname(arg):
+        pass
+
+for num in [1, 2, 3]
+    print90
